6c5aabe7f57067a14307dcd5f90b1d1c7bf07745
aarian <aarian.nayak@gmail.com>
2020-01-18 21:36:31 -0800

    commit

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScrollSpyModule } from 'ngx-scrollspy';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LayoutComponent],
+  declarations: [HeaderComponent, LayoutComponent],
   imports: [
     BrowserModule,
     CommonModule,
